27b8ccda65708fa70dd7d27eb40b93f26de3c155
Tri Vuong <tri.vuong@apple.com>
2020-04-12 00:05:10 +0700

    add some css

--- src/App.js
+++ src/App.js
@@ -4,8 +4,25 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App" style={{textAlign: "center"}}>
-      <h1>COMING SOON</h1>
+    <div style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        height: "100%",
+      }}>
+      <div>
+        <img src="https://img.icons8.com/emoji/96/000000/construction-emoji.png" />
+        {" "}
+        <img src="https://img.icons8.com/color/96/000000/tow-truck.png" />
+        {" "}
+        <img src="https://img.icons8.com/color/96/000000/garbage-truck.png"/>
+        {" "}
+        <img src="https://img.icons8.com/color/96/000000/construction-trowel--v1.png"/>
+      </div>
+      <h1 style={{fontWeight: 500}}>
+        TRIVUONG.DEV IS UNDER CONSTRUCTION
+      </h1>
     </div>
   );
 }
