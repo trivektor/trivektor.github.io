24716e6716a858c595edf82fa9835b1a4c01222f
Tri Vuong <tri.vuong@apple.com>
2020-04-11 22:01:07 +0700

    Updates

---
+++ precache-manifest.88ae6703fa938d8b6d4c9d0b40da978d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f19bc0e139a561bbae2f5b120998c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ea1af67fcb692a77dce",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea8ca933f9efddb1cc51",
+    "url": "/static/js/2.f00df5ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f00df5ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea1af67fcb692a77dce",
+    "url": "/static/js/main.4d16ba60.chunk.js"
+  },
+  {
+    "revision": "83de246caaa99ffc3e8a",
+    "url": "/static/js/runtime-main.44e07a3f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
