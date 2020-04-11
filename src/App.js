23274ffec0494e1ff41b71c9a2125003b0674e8f
Tri Vuong <tri.vuong@apple.com>
2020-04-11 23:31:49 +0700

    add some css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div className="App" style={{textAlign: "center"}}>
+      <h1>COMING SOON</h1>
     </div>
   );
 }
