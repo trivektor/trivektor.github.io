47413a8585f81c6a89457e3e760d8c5bc5cd02d4
Tri Vuong <trivektor@gmail.com>
2022-04-29 14:58:32 +0700

    basic home page

--- components/home.js
+++ components/home.js
@@ -1,5 +1,22 @@
 const Home = () => {
-  return <div className="text-3xl font-bold underline">Hello World</div>;
+  return (
+    <div className="h-screen px-8 py-4">
+      <div>
+        <h1 className="text-4xl my-4">Tri Vuong</h1>
+        <ul>
+          <li>
+            <a href="https://github.com/trivektor">Github</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/tvuong/">LinkedIn</a>
+          </li>
+          <li>
+            <a href="https://twitter.com/trivektor">Twitter</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
