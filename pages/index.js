6aae72f53e9654d8c798135f0dda85ef5497904e
Tri Vuong <trivektor@gmail.com>
2022-05-02 12:44:11 +0700

    add title

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-import Link from "next/link";
+import Head from "next/head";
 import Home from "../components/home";
 
 export default function IndexPage() {
   return (
     <div>
+      <Head>
+        <title>Tri Vuong, JS developer</title>
+      </Head>
       <Home />
     </div>
   );
