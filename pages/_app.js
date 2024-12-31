72735f9f69e931325371405786ab9bde64cead9d
Tri Vuong <trivektor@gmail.com>
2024-12-31 13:20:17 -0800

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-KFCSC6FMS3"
+      ></Script>
+      <Script>
+        window.dataLayer = window.dataLayer || []; function gtag(){" "}
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-KFCSC6FMS3');
+      </Script>
       <Component {...pageProps} />
       <Analytics />
     </>
